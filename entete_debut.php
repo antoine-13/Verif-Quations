79db914d0c97aa8d7c5afa3032a78004b3939e5b
alexiszucher <48800960+alexiszucher@users.noreply.github.com>
2020-05-04 12:18:29 +0200

    04 / 05 / 2020
    Préparation de la page d'index à programmer

--- entete_debut.php
+++ entete_debut.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html>
-  <head>
-      <title>Accueil</title>
-      <meta name="viewport" content="width=device-width,initial-scale=1.0">
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-  </head>
-  <body>
+<!-- Debut d'un fichier incluant les sources CSS de materialize -->
+<?php include_once("debut_fichier.php");?>
 
 
+<!-- Menu bleu en haut du site -->
   <header class="">
       <div class="navbar-fixed">
           <nav style="height:150px;" class="nav-wrapper blue">
@@ -28,14 +22,3 @@
   <ul class="sidenav" id="mobile-demo">
       <!-- Eléments du menu avec le side-nav -->
   </ul>
-
-  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-  <script>
-      $(document).ready(function(){
-          $('.sidenav').sidenav();
-      });
-  </script>
-
-  </body>
-</html>
