38e28916609f30901cd4118ba1d1e8cec5429956
alexiszucher <48800960+alexiszucher@users.noreply.github.com>
2020-05-04 11:49:39 +0200

    entete-debut.php 04 / 05 / 2020

---
+++ entete_debut.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- created by OSTON CODE CYPHER -->
+    <!--Support/subscribe my channel @ youtube.com/ostoncodecypher -->
+      <title>Register</title>
+      <meta name="viewport" content="width=device-width,initial-scale=1.0">
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  </head>
+  <body>
+
+
+  <header class="">
+      <div class="navbar-fixed">
+          <nav class="nav-wrapper blue">
+              <div class="container">
+                  <a href="#!" class="brand-logo center">Vérif&Quations</a>
+                  <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                  <ul class="right hide-on-med-and-down">
+                      <!-- Eléments du menu -->
+                  </ul>
+              </div>
+          </nav>
+      </div>
+  </header>
+
+  <ul class="sidenav" id="mobile-demo">
+      <!-- Eléments du menu avec le side-nav -->
+  </ul>
+
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  <script>
+      $(document).ready(function(){
+          $('.sidenav').sidenav();
+      });
+  </script>
+
+  </body>
+</html>
