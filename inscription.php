7916800f3a05b35186f069c9a7b16a02dab3314e
alexiszucher <48800960+alexiszucher@users.noreply.github.com>
2020-05-04 12:26:54 +0200

    Create inscription.php

---
+++ inscription.php
@@ -0,0 +1,13 @@
+<!--Entete de début du site - menu bleu -->
+<?php include_once("entete_debut.php"); ?>
+
+
+
+<!-- PROGRAMMER ICI -->
+
+
+
+
+
+<!-- Fin d'un fichier incluant les sources JS de materialize -->
+<?php include_once("fin_fichier.php"); ?>
