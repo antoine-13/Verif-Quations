9821b1af787e73cdd5241753fb80703fe9c79a6f
antoine-13 <62340674+antoine-13@users.noreply.github.com>
2020-05-07 21:30:37 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,28 @@
 var number = 1
 function addinput(number, ligne){
+
+    var select = "." + ligne;
+
+    var div = document.createElement("div");
+    div.className="container_chevron col s1";
+    div.style = "height: 100%";
+    div.style.paddingTop = "20px";
+    document.querySelector(select).appendChild(div);
+
+    var i = document.createElement("i");
+    i.className = "material-icons";
+    i.innerHTML = "chevron_right";
+    i.style.height = "100%";
+    document.querySelector(select + " .container_chevron").appendChild(i);
+
+
+    var div = document.createElement("div");
+    div.className = "container_input col s8";
+    document.querySelector(select).appendChild(div)
+
     var input = document.createElement("input");
     input.type = "text";
+    input.name = number;
     switch(number){
         case 1:
             input.placeholder = "Entrez votre " + number + "ere ligne";
@@ -9,21 +30,34 @@ function addinput(number, ligne){
         default:
             input.placeholder = "Entrez votre " + number + "eme ligne";
     }
-    var div = document.createElement("div");
-    div.id = "check";
-    div.style = "position: absolute;";
-    var a = document.createElement("a");
-    a.id = "valid"
-    var i = document.createElement("i");
-    i.className = "material-icons";
-    i.innerHTML = "check";
-    var select = "." + ligne;
 
-    document.querySelector(select).appendChild(input);
-    document.querySelector(select).appendChild(div);
-    document.getElementById("check").appendChild(a);
-    document.getElementById("valid").appendChild(i);
+    document.querySelector(select + " .container_input").appendChild(input);
 
+    var num_ligne_prec = number - 1;
+    var a = num_ligne_prec.toString();
+    var ligne_prec = ".ligne" + a;
+    
+    if(num_ligne_prec >= 1){
+        var span = document.createElement("span");
+        span.innerHTML = document.querySelector(ligne_prec + " .container_input input").value;
+
+        document.querySelector(ligne_prec + " .container_chevron").style.paddingTop = "0";
+
+        var parent = document.querySelector(ligne_prec + " .container_input");
+        var child = document.querySelector(ligne_prec + " .container_input input");
+        parent.replaceChild(span ,child);
+
+        /*var div = document.createElement("div");
+        div.className = "row container_titre";
+        document.querySelector("entree").appendChild(div);
+
+        var span = document.createElement("span");
+        span.innerHTML = "ligne " + a + ":"
+        document.querySelector(ligne_prec + " .container_titre").appendChild(span);*/
+        
+    }
+    
+    
 }
 
 function addrow(number){
@@ -39,3 +73,5 @@ document.getElementById("add").onclick = function() {
     addrow(number);
     number++;
 }
+
+
--- includes/tester_son_equation.php
+++ includes/tester_son_equation.php
@@ -5,20 +5,21 @@
 
 <!-- PROGRAMMER ICI -->
 <main>
-    <div class="row">
-        <div class="col s6 main_box">
-            <div class="row  z-depth-3 equation">
-                <div class="row titre">
-                    <span class="main_text">Entrez votre équation à résoudre :</span>
-                </div>
-                <div class="row ">
-                    <div class="input-field col s6">
-                        <input id="equation" type="text" class="validate">
-                        <label for="equation">Equation</label>
+    <div class="row main_container">
+        <form method = "post">
+            <div class="col s6 main_box">
+                <div class="row  z-depth-3 equation">
+                    <div class="row titre">
+                        <span class="main_text">Entrez votre équation à résoudre :</span>
+                    </div>
+                    <div class="row ">
+                        <div class="input-field col s6">
+                            <input id="equation" type="text" class="validate" name="equation"> 
+                            <label for="equation">Equation</label>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <form>
+                
                 <div class="row  z-depth-3 reponse">
                     <div class="row ">
                         <span class="main_text">Ma réponse :</span>
@@ -35,16 +36,27 @@
                     </div>
                     <div class="row">
                         <div class="col s3 offset-s9">
-                            <button class="btn waves-effect waves-light" type="submit" name="action">Verifier
+                            <button class="btn waves-effect waves-light" type="submit" name="submit">Verifier
                                 <i class="material-icons right">send</i>
                             </button>
                         </div>
                     </div>
                 </div>
-            </form>
+        </form>
 
         </div>
         <div class="col s6 z-depth-3 solution">
+            <?php
+                if(isset($_POST["submit"])){
+                    echo "Votre équation est : ";
+                    echo $_POST["equation"];?> </br> <?php
+                    $number = 1;
+                    while(isset($_POST[$number])){
+                        echo $_POST[$number]; ?></br> <?php
+                        $number = $number + 1;
+                    }
+                }
+            ?>
             
         </div>
 
